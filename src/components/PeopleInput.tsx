5e295159191a4d281a75dfaf3f07b9210d2003df
Pantelis Eleftheriadis <panteliselef@outlook.com>
2023-02-06 21:38:57 +0200

    Added dark mode + colored options in select

--- src/components/PeopleInput.tsx
+++ src/components/PeopleInput.tsx
@@ -1,3 +1,4 @@
+import clx from "classnames";
 import type { KeyboardEventHandler } from "react";
 import { useId, useState } from "react";
 
@@ -5,7 +6,7 @@ import CreatableSelect from "react-select/creatable";
 import { usePeople } from "../contexts/PeopleContext";
 import { getRandomColor } from "../utils/colors";
 
-const components = {
+const _components = {
   DropdownIndicator: null,
 };
 
@@ -35,7 +36,25 @@ export const PeopleInput = () => {
     <CreatableSelect
       id={id}
       className="w-full"
-      components={components}
+      unstyled
+      classNames={{
+        container: () => clx("border dark:border-zinc-700 rounded"),
+        control: () =>
+          clx("bg-white dark:bg-zinc-900 dark:text-white text-sm px-2 rounded"),
+        multiValue: ({ data }) =>
+          clx(`py-1 px-1.5 flex gap-1 rounded`, {
+            "bg-pink-50 text-pink-700": data.color === "pink",
+            "bg-red-50 text-red-700": data.color === "red",
+            "bg-orange-50 text-orange-700": data.color === "orange",
+            "bg-amber-50 text-amber-700": data.color === "amber",
+            "bg-emerald-50 text-emerald-700": data.color === "emerald",
+            "bg-cyan-50 text-cyan-700": data.color === "cyan",
+            "bg-indigo-50 text-indigo-700": data.color === "indigo",
+          }),
+
+        valueContainer: () => clx("flex gap-2"),
+      }}
+      components={_components}
       inputValue={inputValue}
       isClearable
       isMulti
@@ -43,7 +62,7 @@ export const PeopleInput = () => {
       onChange={(newValue) => setPeople(newValue)}
       onInputChange={(newValue) => setInputValue(newValue)}
       onKeyDown={handleKeyDown}
-      placeholder="Type something and press enter..."
+      placeholder="Type your pal's name and press enter..."
       value={people}
     />
   );
