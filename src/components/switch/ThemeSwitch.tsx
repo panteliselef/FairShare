d05ae3892e16848126174fd77db21fce5d243d00
BabisDigos <ntigos2001@gmail.com>
2023-02-10 13:32:43 +0200

    Added dark/light mode switch

---
+++ src/components/switch/ThemeSwitch.tsx
@@ -0,0 +1,14 @@
+import styles from "./theme-switch.module.css";
+import clx from "classnames";
+import { useTheme } from "@contexts/ThemeContext";
+
+export const ThemeSwitch = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <label className={clx(styles.switch)}>
+      <input type="checkbox" onClick={() => toggleTheme()} checked={theme === "light"} />
+      <span className={clx(styles.slider)} />
+    </label>
+  );
+};
